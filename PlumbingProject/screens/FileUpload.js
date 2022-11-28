68c0fbfe9ddd8acc10203124a789a6e298aa0e15
Benen <benensullivan8@gmail.com>
2022-11-28 13:46:01 -0500

    FileUpload Page

---
+++ PlumbingProject/screens/FileUpload.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import DocumentPicker from "react-native-document-picker"
+import {Icon, List, ListItem, Text} from "native-base"
+
+const FileUpload = (props) => {
+    return (
+        <Container>
+            
+        </Container>
+    );
+};
+
+export default FileUpload
