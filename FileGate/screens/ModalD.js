8e83a2ecfb4c9872df8abf3430fc989151fa8471
rthayashi <rh4007@gmail.com>
2023-02-03 14:27:00 -0500

    Update ModalD.js

--- FileGate/screens/ModalD.js
+++ FileGate/screens/ModalD.js
@@ -129,45 +129,14 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     textAlign: 'center',
   },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-},
-inputContainer: {
-    width: '80%'
-},
-input: {
+  input: {
     backgroundColor: 'white',
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 10,
     marginTop: 5,
 },
-buttonContainer: {
-    width: '60%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 40,
-},
-button1: {
-    backgroundColor: '#0782F9',
-    width: '100%',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-},
-buttonOutline: {
-    backgroundColor: 'white',
-    marginTop: 5,
-    borderColor: '#0782F9',
-    borderWidth: 2,
-},
-buttonText: {
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 16,
-}
+  
 });
 
 export default ModalD;
